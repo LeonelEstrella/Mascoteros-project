c88007459ebdfdf8b1932bf20a1c903707f3fb12
Leonel Estrella <ealanleonel@gmail.com>
2021-06-01 20:53:24 -0300

    Metodos para el menu & cambio de nombre

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Menu menu = new Menu();
-        menu.Select_Option();
+        menu.Menu_Interact();
     }
+
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,12 +1,21 @@
+
+import java.io.IOException;
+import java.io.InterruptedIOException;
 import java.util.Scanner;
 
-public class Menu {
-
+public class Menu{
+    private boolean exit= false;
     public Menu(){}
 
-    public void Select_Option()
+    public void Menu_Interact(){
+
+        this.Show_First_Menu();
+        this.Choose(this.Select_Option());
+
+    }
+
+    private void Show_First_Menu()
     {
-        Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese a cual módulo desea ingresar: ");
         System.out.println("1) Productos \n" +
                 "2) Clientes/Proveedores \n" +
@@ -14,8 +23,47 @@ public class Menu {
                 "4) Pedidos \n" +
                 "5) Stock \n" +
                 "6) Salir");
-        int opcion = sc.nextInt();
-
     }
+    private int  Select_Option()
+    {
+        Scanner sc = new Scanner(System.in);
+        int option = sc.nextInt();
+        return option;
+    }
+
+    private void Choose(int option){
+        while (!exit)
+        {
+            switch (option)
+            {
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                case 6:
+                    exit = true;
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("No ingresaste un número válido");
+                    this.Show_First_Menu();
+                    this.Choose(Select_Option());
+            }
+
+        }
+    }
+
 
 }
