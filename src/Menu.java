f4e15bb79e9810da4deb99c135432edb0cab4d5f
Leonel Estrella <ealanleonel@gmail.com>
2021-06-01 19:34:37 -0300

    Añadir primera parte del menu & iniciarlo desde el main

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Menu {
+
+    public Menu(){}
+
+    public void Select_Option()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese a cual módulo desea ingresar: ");
+        System.out.println("1) Productos \n" +
+                "2) Clientes/Proveedores \n" +
+                "3) Telas \n" +
+                "4) Pedidos \n" +
+                "5) Stock \n" +
+                "6) Salir");
+        int opcion = sc.nextInt();
+
+    }
+
 }
