0302a30af5a42879cf95cb49562a27239e9427c1
Leonel Estrella <ealanleonel@gmail.com>
2021-06-05 12:37:20 -0300

    Cambiar nombre en el menu de seleccion

--- src/Menu.java
+++ src/Menu.java
@@ -15,7 +15,7 @@ public class Menu{
         System.out.println("Ingrese a cual módulo desea ingresar: ");
         System.out.println("1) Productos \n" +
                 "2) Clientes/Proveedores \n" +
-                "3) Telas \n" +
+                "3) Textiles \n" +
                 "4) Pedidos \n" +
                 "5) Stock \n" +
                 "6) Salir");
