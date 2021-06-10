9c1cd1b442df0cf8b39450dba7efff1c6d7cc1e9
Leonel Estrella <ealanleonel@gmail.com>
2021-06-09 22:47:03 -0300

    Implementación del patrón singleton para la creación de la clase & crear lógica de la creación de un objeto

--- src/Textiles.java
+++ src/Textiles.java
@@ -1,23 +1,39 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Textiles {
-    private String type, stamp;
-    private int weight, Long, width, idNext=1;
+    private String type, stamp, colour;
+    private int weight, Long, width;
+    private static int idNext=1;
     private boolean thereAreCloth = false;
-    private static int ID=0;
+    private int ID;
     private ArrayList<Textiles> textiles_list = new ArrayList<Textiles>();
+    static private Textiles textiles = null;
+
+    //Implementacion patron Singleton
+    private  Textiles(){}
+
+    static public Textiles getTextiles()
+    {
+        if (textiles == null)
+        {
+            textiles = new Textiles();
+        }
+        return textiles;
+    }
 
     //Quedaría mejor con un método para construir esto?
-    public Textiles(String type, String stamp, int Long, int width, int weight)
+    public Textiles(String type, String stamp,String colour, int Long, int width, int weight)
     {
-        ID= idNext;
-        idNext = idNext + 1;
         this.type = type;
         this.stamp = stamp;
+        this.colour = colour;
         this.Long = Long;
         this.width = width;
         this.weight = weight;
         thereAreCloth = true;
+        ID= idNext;
+        idNext++;
     }
 
     private int Get_ID()
@@ -44,6 +60,10 @@ public class Textiles {
     {
         this.stamp = stamp;
     }
+
+    private String Get_Textiles_Colour(){return colour;}
+
+    private void Set_Textiles_Colour(String colour){ this.colour = colour;}
 
     private int Get_Textiles_Long()
     {
@@ -92,12 +112,35 @@ public class Textiles {
         return textiles_list;
     }
 
-    public void Get_Textiles_List(ArrayList<Textiles> textiles_list)
+    //Ver si el largo y ancho deberia ser double
+    public void Set_Textiles_Object()
     {
-        for (Textiles t: textiles_list)
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese a continuación la información básica de su textil:");
+        System.out.print("1) Tipo: ");
+        String type = sc.nextLine();
+        System.out.print("2) Estampa: ");
+        String stamp = sc.nextLine();
+        System.out.print("3) Color: ");
+        String colour = sc.nextLine();
+        System.out.print("4) Largo: ");
+        int Long = sc.nextInt();
+        System.out.print("5) Ancho: ");
+        int width = sc.nextInt();
+        System.out.print("6) Peso: ");
+        int weight = sc.nextInt();
+        Textiles test = new Textiles(type,stamp,colour,Long,width,weight);
+        Add_Textiles_List(test);
+    }
+
+    public void Get_Textiles_List(/*ArrayList<Textiles> textiles_list*/)
+    {
+        for (Textiles t: this.textiles_list)
         {
-            System.out.println("El ID del producto es " + t.Get_ID() + ". El tipo de tela es " + t.Get_Textiles_Type()+ ". Su estampa es "+ t.Get_Textiles_Stamp()
-                    + ". El largo actual de la tela es " + t.Get_Textiles_Long() + " metros y su ancho " + t.Get_Textiles_Width() + " metros. Su peso actual es de " + t.Get_Textiles_Weight() + " kilos.");
+            System.out.println("El ID del producto es " + t.Get_ID() + ". El tipo de tela es " + t.Get_Textiles_Type()
+                    + " con color " + t.Get_Textiles_Colour() + " . Su estampa es "+ t.Get_Textiles_Stamp()
+                    + ". El largo actual de la tela es " + t.Get_Textiles_Long() + " metros y su ancho "
+                    + t.Get_Textiles_Width() + " metros. Su peso actual es de " + t.Get_Textiles_Weight() + " kilos.");
         }
     }
 
@@ -109,9 +152,16 @@ class main2
 {
     public static void main(String[] args)
     {
-        Textiles a= new Textiles("polar","huesitos",120,100,20);
-        ArrayList<Textiles> lista = new ArrayList<Textiles>();
+        Textiles a =  Textiles.getTextiles();
+        a.Set_Textiles_Object();
+        a.Set_Textiles_Object();
+        a.Set_Textiles_Object();
+        /*ArrayList<Textiles> lista;
         lista = a.Add_Textiles_List(a);
-        a.Get_Textiles_List(lista);
+        for(int i=0; i<= lista.size(); i++)
+        {
+            if(lista[i]. )
+        }*/
+        a.Get_Textiles_List();
     }
 }
